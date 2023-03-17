38f0a10fb1b7a2ece3ba48a822e33663a25d5c79
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-03-17 14:22:01 -0700

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+int d, p;
+for (d = '0'; d < '89'; d++)
+{
+for (p = d + 1; p <= '99'; p++)
+{
+if (p != d)
+{
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+putchar(' ');
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+if (d == '89' && p == '99')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
