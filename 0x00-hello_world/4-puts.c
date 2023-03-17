a8cb96040c731d0b18d5f3d52a71e937dcb5cb3f
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-03-16 18:15:04 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,11 @@
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline."
- * return: Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
+  
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
