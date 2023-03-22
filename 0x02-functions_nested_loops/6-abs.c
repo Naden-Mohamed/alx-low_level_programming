2efbe7f3bd237679eb5560a833c2692c2b8ac1b5
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-03-21 18:16:48 -0700

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_abs - checks for lowercase character
+*@c: The character to be checked
+*Return: Absolute value of number or zero
+*/
+
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_val;
+abs_val = v * -1;
+return (abs_val);
+}
+return (c);
+}
