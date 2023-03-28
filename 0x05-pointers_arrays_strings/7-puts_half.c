cbf418f81edde22ce95d1de96a52df5a75865634
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-03-27 18:46:25 -0700

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,11 +5,11 @@
  * @str: The string to print
  * Retuen: void
  */
-void puts_half( char *str)
+void puts_half(char *str)
 {
 int j = 0;
 int k;
-while(str[j] != '\0')
+while (str[j] != '\0')
 {
 j++;
 }
