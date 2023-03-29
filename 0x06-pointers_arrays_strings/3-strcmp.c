a09a1411482529998a7b5ad288b8dd56d64f4a84
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-03-29 14:34:50 -0700

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,13 @@
  * if str1 > str2, the positive difference of the first unmatched characters.
  *
  */
- 
- int _strcmp(char *s1, char *s2)
- {
- while (*s1 && *s2 && *s1 == *s2)
- {
- s1++;
- s2++;
- }
- return (*s1 - *s2);
- }
 
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && *s2 && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+return (*s1 - *s2);
+}
