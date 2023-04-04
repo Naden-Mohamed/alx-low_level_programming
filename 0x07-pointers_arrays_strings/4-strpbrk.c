6af68c48c851c84740a2c2aa959a56f4363dcc26
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-04-03 19:25:32 -0700

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,12 +7,13 @@
  * @accept: input
  * Return: Always 0 (Success)
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 int i, n;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (n = 0; check[n] != '\0'; n++)
+for (n = 0; accept[n] != '\0'; n++)
 {
 if (s[i] == accept[n])
 {
