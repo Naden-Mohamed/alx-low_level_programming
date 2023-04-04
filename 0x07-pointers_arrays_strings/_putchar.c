ae24b2a35f7d2f69d6c424505b9193afe5af2141
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-04-03 18:38:37 -0700

    Update _putchar.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,8 +1,9 @@
 #include<unistd.h>
 
 /**
-*_putchar - write the character c to stdout
+*_putchar - writes the character c to stdout
 *@c: The character to print
+*
 *Return on success 1.
 *On error, -1 is returned, and errno is set appropriatly.
 */
