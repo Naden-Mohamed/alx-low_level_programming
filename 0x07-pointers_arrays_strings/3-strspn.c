6a2a992a62727fb393c4389f05df5c6876691e89
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-04-03 19:27:05 -0700

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 unsigned int i, n, check;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (n = 0; check[n] != s[i]; n++)
+for (n = 0; accept[n] != s[i]; n++)
 {
 if (accept[n] = '\0')
 {
