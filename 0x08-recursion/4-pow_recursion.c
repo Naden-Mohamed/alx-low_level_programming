b421c01a3b0033685e75c542d25154c700a576e9
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-04-05 16:22:37 -0700

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _pow_recursion - Returns the value of x raised to the power of y
  * @x: the value to multiply
- * @y: the times to multiply the value 
+ * @y: the times to multiply the value
  *
  * Return: the value multiplied y times
  */
@@ -12,5 +12,5 @@ if (y < 0)
 return (-1);
 if (y == 0)
 return (1);
-return (x *  _pow_recursion(x, y- 1));
+return (x *  _pow_recursion(x, y - 1));
 }
