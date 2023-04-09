bca58e6be818450a954c320add80ba8fe83b6884
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-04-09 10:22:45 -0700

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 /**
- * main - Prints the multiplication of two args numbers
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: Always zero
- */
+  * main - Prints the multiplication of two args numbers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 {
 int n1 = 0, n2 = 0;
@@ -20,7 +20,7 @@ printf("%d\n", n1 * n2);
 }
 else
 {
-printf("ERROR\n");
+printf("Error\n");
 return (1);
 }
 return (0);
