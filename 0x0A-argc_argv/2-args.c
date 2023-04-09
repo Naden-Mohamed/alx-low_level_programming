bf4e9806627a23684f8776b19578eade7a2e936d
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-04-09 10:03:10 -0700

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,12 @@
-#include <stdio.h>	
 #include "main.h"
+#include <stdio.h>	
 
 /**
  * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: Always zero
  */	
 int main(int argc, char *argv[])
 {
