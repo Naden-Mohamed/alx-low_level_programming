4fbee42b23a10d1d5d4c1dd3eb057fd562108fa5
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-04-11 16:58:33 -0700

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
   * str_concat - Concatenates two strings of any size
-  * @s1: the first string to concatenate 
+  * @s1: the first string to concatenate
   * @s2: the second string to concatenate
   *
   * Return: The two strings concatenated
@@ -15,15 +15,15 @@ char *s;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
-s2 = ""; 
+s2 = "";
 while (s1[i])
 i++;
 while (s2[j])
 j++;
 l = i + j;
-s = malloc((sizeof(char) * i) +1);  
+s = malloc((sizeof(char) * i) +1);
 if (s == NULL)
-return (NULL);  
+return (NULL);
 j = 0;
 while (k < l)
 {
