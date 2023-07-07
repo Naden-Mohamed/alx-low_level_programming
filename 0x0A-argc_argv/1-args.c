8e486cbf196bc8f639b01dc4e7765ff7c2b7753a
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-07-08 00:00:22 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * 
+ * Return: Always zero
+ */
+int main (int argc, char *argv[])
+{
+(void) argc;
+printf("%d\n", argc - 1);
+return (0);
+}
