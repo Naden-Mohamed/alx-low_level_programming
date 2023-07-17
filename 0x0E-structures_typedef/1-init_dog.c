23f0f564c1f2a813e81c8852882c51f3b7c36bd1
Naden-Mohamed <125380572+Naden-Mohamed@users.noreply.github.com>
2023-07-17 21:29:20 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,19 @@
-#ifndef DOG_H
-#define DOG_H
+#include"dog.h"
 /**
- * struct dog - a dog struct
+ * init_dog - initializes a dog
+ * @d: the dog to init
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
  *
- * Description: just a lone dog struct in a big kitty world
+ * Return: avoid.
  */
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-char *name;
-float age;
-char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
